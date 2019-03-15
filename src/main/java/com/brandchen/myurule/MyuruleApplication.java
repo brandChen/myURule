161d31a007eae3bb77d79eff3f12d89464cc57c3
Martin <1923332150@qq.com>
2019-03-15 11:05:34 +0800

    feat: project init

---
+++ src/main/java/com/brandchen/myurule/MyuruleApplication.java
@@ -0,0 +1,13 @@
+package com.brandchen.myurule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyuruleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyuruleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandchen/myurule/MyuruleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brandchen.myurule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyuruleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
